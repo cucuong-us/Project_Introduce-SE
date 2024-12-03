270824f61f6bf82cd79dd67a52a45cdb7ab6946e
DatNewBie <thanhdattqc@gmail.com>
2024-12-04 00:12:29 +0700

    (maybe) done all api

--- src/main/java/com/introduce_SE/demo/ClinicWeb/service/MedicineService.java
+++ src/main/java/com/introduce_SE/demo/ClinicWeb/service/MedicineService.java
@@ -1,5 +1,6 @@
 package com.introduce_SE.demo.ClinicWeb.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,5 +17,38 @@ public class MedicineService {
 	
 	public Optional<Medicine> findById(int id_medicine) {
 		return medicineRepository.findById(id_medicine);
+	}
+	
+	public List<Medicine> findAll() {
+		return medicineRepository.findAll();
+	}
+	
+	public Medicine save(Medicine medicine) {
+		return medicineRepository.save(medicine);
+	}
+	
+	public void deleteById(int id) {
+		medicineRepository.deleteById(id);
+	}
+	
+	public Medicine updateById(int id, Medicine medicine) {
+		Optional<Medicine> m = medicineRepository.findById(id);
+		if(m.isEmpty()) {
+			return m.get();
+		}
+		
+		if(medicine.getNameOfMedicine() != null) 
+			m.get().setNameOfMedicine(medicine.getNameOfMedicine());
+		
+		if(medicine.getMedicineUsage() != null)
+			m.get().setMedicineUsage(medicine.getMedicineUsage());
+		
+		if(medicine.getPrice() != 0) 
+			m.get().setPrice(medicine.getPrice());
+		
+		if(medicine.getUnit() != null)
+			m.get().setUnit(medicine.getUnit());
+		
+		return medicineRepository.save(m.get());
 	}
 }
